526a0591e088f791ccd2f8b86f0fb475c3ca47dc
vqlinh <vuongquanglinh1234@gmail.com>
2021-12-30 12:23:23 +0700

    resole config

--- register_submit.php
+++ register_submit.php
@@ -2,7 +2,6 @@
     session_start();
 
     include'config.php';
-<<<<<<< HEAD
     if(isset($_POST['submit']) && $_POST["username"] != '' && $_POST["fullname"] != '' && $_POST["name"] != '' && $_POST["password"] != ''  )
     {   
         $username=$_POST["username"];
@@ -18,33 +17,11 @@
             $_SESSION["thongbao"]="Tài khoản đã tồn tại";
             echo '<script>alert("Image Inserted into Database")</script>';  
 
-=======
-    if(isset($_POST['submit']) && $_POST["username"] != '' && $_POST["email"] != '' && $_POST["password"] != '' && $_POST["password2"] != '' )
-    {
-        $username=$_POST["username"];
-        $email=   $_POST["email"];
-        $password=$_POST["password"];
-        $password2=$_POST["password2"];
-        if($password != $password2) {
-            $_SESSION["thongbao"] ="mật khẩu như cái chân gà vậy, KHÔNG KHỚP";
-
-            header("location:register.php");
-            die();
-        }
-        $sql = "SELECT * from user WHERE username='$username'";
-        $result=mysqli_query($conn,$sql);
-        if(mysqli_num_rows($result) > 0 ) {
-            $_SESSION["thongbao"]="Tài khoản đã tồn tại";
->>>>>>> a896fbdf5b4d1f6aa68e0f009f2a4a654a6d9219
      
             header("location:register.php");   
             die();
         }
-<<<<<<< HEAD
         $sql="INSERT INTO user_accounts (username, fullname,name, password,image ) VALUES ('$username','$fullname','$name','$password','$file')";
-=======
-        $sql="INSERT INTO user (username, email, password ) VALUES ('$username','$email','$password')";
->>>>>>> a896fbdf5b4d1f6aa68e0f009f2a4a654a6d9219
         mysqli_query($conn,$sql);
         $_SESSION["thongbao"]=" Đã đăng kí thành công";
         header("location:login.php");
