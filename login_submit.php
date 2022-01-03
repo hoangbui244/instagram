078ffd89ab749b711048b21842f40df39caf170d
Pham Van Khai <phamvankhai020601@gmail.com>
2022-01-03 11:23:00 +0700

    password_hash

--- login_submit.php
+++ login_submit.php
@@ -3,29 +3,27 @@
     include'config.php';
     if(isset($_POST['submit']) && $_POST["username"] !='' && $_POST["password"] !=''   )
     {   
-        
         $username = $_POST["username"];
         $password = $_POST["password"];
-        $sql = "SELECT * FROM user_account WHERE username='$username' ";
-        $query = mysqli_query($conn,$sql);
-        $row = mysqli_fetch_array($query);     
-        $count = mysqli_num_rows($query);
-        // echo $password;
-        // echo "xuogn dong    ";
-        // echo $row["user_password"];
-        if($count > 0){
-         
+        $sql = "SELECT * FROM user_account WHERE username=?";
+        //$query = mysqli_query($conn,$sql);
+        $stmt = mysqli_prepare($conn,$sql);
+        mysqli_stmt_bind_param($stmt, "s", $username);
+        //$row = mysqli_fetch_array($query);     
+        //$count = mysqli_num_rows($query);
+        if(mysqli_stmt_execute($stmt)){
+            mysqli_stmt_bind_result($stmt,$id,$email,$user_password,$username,$avatar);
+            if(mysqli_stmt_fetch($stmt)){
                 $_SESSION["loged"] = true;
-                $_SESSION["username_id"] = $row["id"];
-                $_SESSION["username"] = $row["username"];
-                $_SESSION["avatar"] = $row["avatar"];
-                header("location:index.php");
-       
-            
-    }
-    }
-    else{
-        $_SESSION["thongbao"]="Vui lòng nhập đầy đủ thông tin";
-        header("location:login.php");
+                $_SESSION["username_id"] = $id;
+                $_SESSION["username"] = $username;
+                $_SESSION["avatar"] = $avatar;
+                header("location:index.php");   
+            }
+            else{
+                echo "loi roi";
+            }
+        }
+
     }
 ?>
