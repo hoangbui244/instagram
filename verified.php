b39ddde53f2b750b60676bfb5839577afd1847e2
Pham Van Khai <phamvankhai020601@gmail.com>
2022-01-03 22:03:17 +0700

    done vertifi email

--- verified.php
+++ verified.php
@@ -5,14 +5,9 @@
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
 	</head>
 <body>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="https://sourcecodester.com">Sourcecodester</a>
-		</div>
-	</nav>
 	<div class="col-md-3"></div>
 	<div class="col-md-6 well">
-		<h3 class="text-primary">PHP - Simple Email Verification Using PHPMailer</h3>
+		<h3 class="text-primary">Instagram</h3>
 		<hr style="border-top:1px dotted #ccc;"/>
 		<div class="col-md-2"></div>
 		<div class="col-md-8">	
