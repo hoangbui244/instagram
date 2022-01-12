928e986cafff18ec4dbc656f4684a41b8f27b44d
tiramiSoo <ntdung1228@gmail.com>
2022-01-12 12:45:41 +0700

    admin

--- admin/admin.php
+++ admin/admin.php
@@ -13,7 +13,41 @@
     <link rel="stylesheet" href="css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css"></script>
     <title>Document</title>
+    <script>
+        $(document).ready(function() {
+     
+     // Cấu hình các nhãn phân trang
+     $('#example').dataTable( {
+         "language": {
+         "sProcessing":   "Đang xử lý...",
+         "sLengthMenu":   "Xem _MENU_ mục",
+         "sZeroRecords":  "Không tìm thấy dòng nào phù hợp",
+         "sInfo":         "Đang xem _START_ đến _END_ trong tổng số _TOTAL_ mục",
+         "sInfoEmpty":    "Đang xem 0 đến 0 trong tổng số 0 mục",
+         "sInfoFiltered": "(được lọc từ _MAX_ mục)",
+         "sInfoPostFix":  "",
+         "sSearch":       "Tìm:",
+         "sUrl":          "",
+         "oPaginate": {
+             "sFirst":    "Đầu",
+             "sPrevious": "Trước",
+             "sNext":     "Tiếp",
+             "sLast":     "Cuối"
+             }
+         }
+     } );
+          
+     } ); 
+    </script>
 </head>
 
 <body>
@@ -74,43 +108,25 @@
                         </a>
                 </div>  
             </div>
-            <div class="main_right container">
-                <div class="search">
-                    <div class="icon"></div>
-                    <div class="input">
-                        <input type="text" placeholder="Tìm kiếm" id="mysearch"> 
-                    </div>
-                    <span class="clear" onclick="document.getElementById('mysearch').value = '' "></span>
-                </div>
+            <div class="main_right container">               
                 <div class ="tab-content">
                     <div id="manager_user" class="tabcontent">                  
-                        <table id= "table-id" class="tablecontent ">
+                    <table id="example" class="table table-striped table-bordered" style="width:100%">
                             <thead>
                                 <tr>
                                   <th>User ID</th>
                                   <th>Username</th>
                                   <th>Email</th>
                                   <th>Status</th>
-                                  <th>Report Count</th>
-                                  
+                                  <th>Report Count</th>                                  
                                 </tr>
                               </thead>                              
-                              <tbody>
-                              <?php 
-                                if(isset($_GET['page']))
-                                {
-                                    $page_user = $_GET['page'];
-                                }
-                                else
-                                {
-                                $page_user = 1;
-                                }
-                                $num_per_page_user = 5;
-                                $start_from_user = ($page_user-1) * 5;                             
-                                $sql_get_user = "select * from user_account limit $start_from_user, $num_per_page_user";
+                              <tbody>  
+                              <?php
+                                $sql_get_user = "select * from user_account";
                                 $query_user = mysqli_query($conn, $sql_get_user);
                                  while($pro = mysqli_fetch_assoc($query_user)):
-                              ?>
+                              ?>                           
                                 <tr>
                                   <td><?php echo $pro["id"] ?></td>
                                   <td><?php echo $pro["username"] ?></td>
@@ -120,86 +136,33 @@
                                  </tr>   
                                  <?php endwhile ?>                                                      
                               </tbody>                                         
-                          </table>
-                          <?php
-                          
-                            $sql_get_user = "select * from user_account";
-                            $query_user = mysqli_query($conn, $sql_get_user);
-                            $total_record_user = mysqli_num_rows($query_user);
-            
-                            $total_page_user = ceil($total_record_user/$num_per_page_user);
-
-                            if($page_user>1)
-                            {
-                                echo "<a href='admin.php?page=".($page_user-1)."' class='btn btn-danger'><</a>";
-                            }
-            
-                            for($i=1;$i<$total_page_user;$i++)
-                            {
-                                echo "<a href='admin.php?page=".$i."' class='btn btn-primary'>$i</a>";
-                            }
-                            if($i>$page_user)
-                            {
-                                echo "<a href='admin.php?page=".($page_user+1)."' class='btn btn-danger'>></a>";
-                            }
-                        ?>
+                          </table>                         
                     </div>
                     <div id ="manager_post" class="tabcontent">
-                        <table id="table-id" class="tablecontent">
-                            <thead>
-                                <tr>
-                                  <th >ID</th>
-                                  <th >User ID</th>
-                                  <th >Report Count</th>
-                                  <th >Report Content</th>
-                                </tr>
-                              </thead>
-                            <tbody>
-                            <?php        
-                                if(isset($_GET['page']))
-                                {
-                                    $page_posts = $_GET['page'];
-                                }
-                                else
-                                {
-                                $page_posts = 1;
-                                }
-                                $num_per_page_posts = 5;
-                                $start_from_posts = ($page_posts-1) * 5;                                
-                                $sql_get_posts = "select * from posts limit $start_from_posts, $num_per_page_posts";
+                    <table id="example" class="table table-striped table-bordered" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>User ID</th>
+                                <th>Report Count</th>
+                                <th>Report Content</th>                       
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                $sql_get_posts = "select * from posts";
                                 $query_posts = mysqli_query($conn, $sql_get_posts);
                                  while($pro = mysqli_fetch_assoc($query_posts)):
                               ?>
-                                <tr>
-                                  <td><?php echo $pro["id"] ?></td>
-                                  <td><?php echo $pro["user_id"] ?></td>
-                                  <td><?php echo $pro["report"] ?></td>
-                                  <td><?php echo $pro["report_content"] ?></td>
-                                 </tr>   
-                                 <?php endwhile ?>
+                            <tr>
+                                <td><?php echo $pro["id"] ?></td>
+                                <td><?php echo $pro["user_id"] ?></td>
+                                <td><?php echo $pro["report"] ?></td>                               
+                                <td><?php echo $pro["report_content"] ?></td>
+                            </tr>
+                            <?php endwhile ?> 
                             </tbody>
                           </table>
-                          <?php                           
-                              $sql_get_posts = "select * from posts";
-                              $query_posts = mysqli_query($conn, $sql_get_posts);
-                              $total_record_posts = mysqli_num_rows($query_posts);
-              
-                              $total_page_posts = ceil($total_record_posts/$num_per_page_posts);
-  
-                              if($page_posts>1)
-                              {
-                                  echo "<a href='admin.php?page=".($page_posts-1)."' class='btn btn-danger'><</a>";
-                              }
-              
-                              for($i=1;$i<$total_page_posts;$i++)
-                              {
-                                  echo "<a href='admin.php?page=".$i."' class='btn btn-primary'>$i</a>";
-                              }
-                              if($i>$page_posts)
-                              {
-                                  echo "<a href='admin.php?page=".($page_posts+1)."' class='btn btn-danger'>></a>";
-                              }
-                          ?>
                 </div>
             </div>
         </div>
